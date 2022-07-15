4d9257b8a25a8a18295036331c97a4fee740513d
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-15 18:34:56 -0300

    asd

--- menu.js
+++ menu.js
@@ -2,5 +2,5 @@
 document.getElementById("menu_icon").addEventListener("click", mostrar_menu);
 
 function mostrar_menu() {
-  document.querySelector(".menu").classList.toggle("mostrar_menu");
+  document.getElementById("menu").classList.toggle("mostrar_menu");
 }
