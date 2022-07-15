8d57d4ca9efa3b9a037ece94ada7f816d6afb7f3
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-15 10:45:18 -0300

    DB added

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$conn = mysqli_connect("localhost","u523579628_marcoszinga","KilaGames1","u523579628_juegos");
+if(!$conn){
+  echo "Connection error: " . mysqli_connect_error();
+}
+
+$pc = "SELECT * FROM `pc`";
+
+?>
