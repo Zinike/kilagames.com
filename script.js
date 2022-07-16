00d7891ffad3bc32620ea1404bb416f9c53f603b
Marcos <marcoszinga_1994@hotmail.com>
2022-07-15 21:08:44 -0300

    asd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //   FUNCION MOSTRAR MENU
-document.querySelector(".menu_icon").addEventListener("click", mostrar_menu);
+document.getElementById(id)("menu_icon").addEventListener("click", mostrar_menu);
 
 function mostrar_menu() {
   document.querySelector(".menu").classList.toggle("mostrar_menu");
