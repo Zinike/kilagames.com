a4d77786309dbb341f0c3e0213301ef1e4bfc1dc
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-16 10:46:32 -0300

    asd

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
     </div>
     <div class="contenedor">
       <div class="discord">
-        <iframe src="https://discord.com/widget?id=980502183546089483&theme=light" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <iframe src="https://discord.com/widget?id=980502183546089483&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </div>
     </div>
   </section>
