f84d583dc2dc540f2d96487ba3ba4cef8c8d615a
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-18 20:54:10 -0300

    asd

--- login.php
+++ login.php
@@ -53,7 +53,7 @@
   <section id="login">
     <div class="contenedor">
       <h2>Ingresa a tu cuenta</h2>
-      <?php include("controlador.php"); ?>
+      
       <form class="form-login" action="index.php" method="post">
         <label for="username">Usuario</label>
         <input type="text" placeholder="Ingresa tu usuario" name="usuario"><br>
