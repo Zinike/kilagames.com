4d9ee5b98e5e745f813ee7b82c8baf3c68b0c945
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-18 20:58:15 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!empty($_POST["login-button"])){
-  if (empty($_POST["usuario"]) and empty($_POST["contraseña"])){
+  if (empty($_POST["usuario"]) and empty($_POST["contraseña"])) {
     echo '<div class="error"><p>RELLENAR LOS CAMPOS</p></div>'
   } else {
     // code...
