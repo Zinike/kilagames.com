256ea1c5a0a3b8c7af100866cd98884a5a1733ea
Marcos <marcoszinga_1994@hotmail.com>
2022-07-18 21:35:36 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -17,16 +17,6 @@ if (isset($_POST["login-button"])){
     } else {
       echo '<h3>Paso algo mal</h3>'
     }
-
-
-    $sql = $conexion->query("SELECT * from  `usuarios` WHERE usuario='$usuario' AND contraseña='$contraseña'");
-    if ($datos=sql->fetch_object()) {
-      header("location:index.php");
-    } else {
-      echo '<div class="error"><p>ACCESO DENEGADO</p></div>';
-    }
-  }
-
 }
-
+}
 ?>
