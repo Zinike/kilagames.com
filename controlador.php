98dfb29ed3f492bc0fe065388220712004d40da2
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-18 21:04:07 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -4,7 +4,14 @@ if (!empty($_POST["login-button"])){
   if (empty($_POST["usuario"]) and empty($_POST["contraseña"])) {
     echo '<div class="error"><p>RELLENAR LOS CAMPOS</p></div>'
   } else {
-    // code...
+    $usuario = $_POST["usuario"];
+    $contraseña = $_POST["contraseña"];
+    $sql = $conexion->query("SELECT * from  `usuarios` WHERE usuario=$usuario AND contraseña=$contraseña");
+    if ($datos=sql->fetch_object()) {
+      header("location:index.php");
+    } else {
+      echo '<div class="error"><p>ACCESO DENEGADO</p></div>';
+    }
   }
 
 }
