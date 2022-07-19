607e15c3ea8514d713740dd26eaef6ba41e0b215
Marcos <marcoszinga_1994@hotmail.com>
2022-07-18 21:28:07 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -5,8 +5,8 @@ if (isset($_POST["login-button"])){
   if (empty($_POST["usuario"]) and empty($_POST["contraseña"])) {
     echo '<div class="error"><p>RELLENAR LOS CAMPOS</p></div>'
   } else {
-    $usuario = $_POST["usuario"];
-    $contraseña = $_POST["contraseña"];
+    $usuario = trim$_POST["usuario"];
+    $contraseña = trim$_POST["contraseña"];
     $sql = $conexion->query("SELECT * from  `usuarios` WHERE usuario='$usuario' AND contraseña='$contraseña'");
     if ($datos=sql->fetch_object()) {
       header("location:index.php");
