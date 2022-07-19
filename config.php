34c3c8142d25d16d92c4e2cd653e46304b653b77
Marcos <marcoszinga_1994@hotmail.com>
2022-07-19 00:59:35 -0300

    asd

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 include("conexion.php");
-if($conn){
-  echo "todo bien";
-}
+
 if (isset($_POST['register'])) {
     if (strlen($_POST['usuario']) >= 1 && strlen($_POST['contraseña']) >= 1) {
 	    $name = trim($_POST['usuario']);
-	    $email = trim($_POST['contraseña']);
+	    $contraseña = trim($_POST['contraseña']);
+
 	    $consulta = "INSERT INTO usuarios(nombre, email) VALUES ('$usuario','$contraseña')";
+
 	    $resultado = mysqli_query($conexion,$consulta);
 	    if ($resultado) {
         echo "todo bien";
