e5c15be80039cf1fd2c2c8cc01b983870bd12d42
Marcos <marcoszinga_1994@hotmail.com>
2022-07-19 00:15:17 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -4,11 +4,11 @@ include("conexion.php");
 
 if (isset($_POST["login-button"])) {
   if (strlen($_POST['nombre']) >= 1 && strlen($_POST['apellido']) >= 1 && strlen($_POST['usuario']) >= 1 && strlen($_POST['contraseña']) >= 1){
-    $nombre = trim($_POST['nombre');
+    /*$nombre = trim($_POST['nombre');
     $apellido = trim($_POST['apellido']);
     $usuario = trim($_POST['usuario']);
     $contraseña = trim($_POST['contraseña']);
-    /*$consulta = "INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`) VALUES (NULL, '$nombre', '$apellido', '$usuario', '$contraseña');"
+    $consulta = "INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`) VALUES (NULL, '$nombre', '$apellido', '$usuario', '$contraseña');"
     $resultado = mysqli_query($conexion,$consulta);
     if ($resultado) {
 	    	?>
