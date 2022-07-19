dfcb430cffd29a82c21c3c0397971c4587648658
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-19 11:53:53 -0300

    asd

--- login.php
+++ login.php
@@ -65,7 +65,7 @@
           <input type="text" name="usuario" placeholder="Ingresa tu usuario"><br>
 
           <label for="password">Contraseña</label>
-          <input type="password" name="contraseña" placeholder="Ingresa tu contraseña">
+          <input type="password" name="contraseña" placeholder="Ingresa tu contraseña"><br>
 
           <input type="submit" name="login-button" value="Entrar"><br>
 
