a60ff6d6129d90d9cf29808f39c355216790b50f
Marcos <marcoszinga_1994@hotmail.com>
2022-07-19 00:41:18 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -9,7 +9,7 @@ if (isset($_POST["register"])) {
     $usuario = trim($_POST['usuario']);
     $contraseña = trim($_POST['contraseña']);
 
-    $consulta = "INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`) VALUES ('NULL', '$nombre', '$apellido', '$usuario', '$contraseña')";
+    $consulta = "INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`) VALUES (NULL, '$nombre', '$apellido', '$usuario', '$contraseña')";
 
     $resultado = mysqli_query($conexion,$consulta);
 
@@ -22,8 +22,7 @@ if (isset($_POST["register"])) {
     	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
             <?php
         }
-
-  }else{
+    } else {
     ?>
     <h3 class="bad">¡COMPLETA LOS CAMPOS!</h3>
     <?php
