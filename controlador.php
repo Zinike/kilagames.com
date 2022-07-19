8d9c6970b6cd4ff6347645964bebeb8ba84f8443
Marcos <marcoszinga_1994@hotmail.com>
2022-07-19 00:07:40 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -18,7 +18,7 @@ if (isset($_POST["login-button"])) {
 	    	?>
 	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
         <?php
-    }else {
+    } else {
 	    	?>
 	    	<h3 class="bad">¡Por favor complete los campos!</h3>
         <?php
