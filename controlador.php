862dfa30db4c6db36cd8223d52f616ba368eae8c
Marcos <marcoszinga_1994@hotmail.com>
2022-07-18 22:40:49 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -1,6 +1,6 @@
 <?php
-include("conexion.php");
 
+include("conexion.php");
 
 if (isset($_POST["login-button"])) {
   if (strlen($_POST['nombre']) >= 1 && strlen($_POST['apellido']) >= 1 && strlen($_POST['usuario']) >= 1 && strlen($_POST['contraseña']) >= 1){
@@ -25,4 +25,5 @@ if (isset($_POST["login-button"])) {
     }
   }
 }
+
 ?>
