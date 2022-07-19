4426bb677706fa10055b063b0dd7d22e88285405
Marcos <marcoszinga_1994@hotmail.com>
2022-07-18 21:32:49 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -7,6 +7,16 @@ if (isset($_POST["login-button"])){
   } else {
     $usuario = trim$_POST["usuario"];
     $contraseña = trim$_POST["contraseña"];
+
+    $consulta = "INSERT INTO `usuarios`(`nombre`, `apellido`, `usuario`, `contraseña`) VALUES ('$nombre','$apellido','$usuario','$contraseña')"
+
+    $resultado = mysqlquery($conexion, $consulta);
+
+    if ($resultado) {
+      echo '<h3>Gracias por unirte!</h3>'
+    }
+
+
     $sql = $conexion->query("SELECT * from  `usuarios` WHERE usuario='$usuario' AND contraseña='$contraseña'");
     if ($datos=sql->fetch_object()) {
       header("location:index.php");
