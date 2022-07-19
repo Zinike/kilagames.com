aff4bf2cff6f9f9c2f5350e6876b7c402ff45d51
Marcos <marcoszinga_1994@hotmail.com>
2022-07-18 22:19:57 -0300

    asd

--- controlador.php
+++ controlador.php
@@ -8,7 +8,7 @@ if (isset($_POST["login-button"])){
     $usuario = trim($_POST['usuario']);
     $contraseña = trim($_POST['contraseña']);
 
-    $consulta = "INSERT INTO usuarios(nombre,apellido,usuario,contraseña) VALUES ('$nombre','$apellido','$usuario','$contraseña')"
+    $consulta = "INSERT INTO usuarios(nombre,apellido,usuario,contraseña) VALUES ('$nombre','$apellido','$usuario','$contraseña')";
 
     $resultado = mysqli_query($conexion,$consulta);
 
