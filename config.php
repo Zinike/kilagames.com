cff83af3f5929f5113b487d5d49902762a10f914
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-20 19:15:08 -0300

    ASD

--- config.php
+++ config.php
@@ -5,21 +5,25 @@ include("conexion.php");
 if (isset($_POST['login-register'])) {
   if (strlen($_POST['usuario']) >= 4 && strlen($_POST['contraseña']) >= 8) {
 
+
     // ELIMINACION DE ESPACIO CON TRIM
     //$usuario = trim($_POST['usuario']);
     //$contraseña = trim($_POST['contraseña']);
+
 
     // ELIMINACION DE INJECCION (" OR 1=1#)
     //$usuario = mysqli_real_escape_string($conn, $usuario);
     //$contraseña = mysqli_real_escape_string($conn, $contraseña);
 
+
     // VERIFICAION DE DUPLICACION DE USUARIO
     //$contejamiento = "SELECT FROM usuarios  WHERE usuario=$usuario";
     //$usuario_verificado = mysqli_query($conexion,$cotejamiento);
 
-    // ENCRIPTACIONB DE CONTRASEÑA
 
+    // ENCRIPTACIONB DE CONTRASEÑA
     //$contraseña = password_hash($contraseña, PASSWORD_BCRYPT);
+
 
     // INGRESO DE USUARIO Y CONTRASEÑA A LA BD
     $consulta = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña')";
