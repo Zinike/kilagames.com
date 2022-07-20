e4d5b3ccd67c31be813b4ded46fe67d671cb317b
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-20 19:27:23 -0300

    asd

--- config.php
+++ config.php
@@ -7,26 +7,26 @@ if (isset($_POST['login-register'])) {
 
 
     // ELIMINACION DE ESPACIO CON TRIM
-    //$usuario = trim($_POST['usuario']);
-    //$contraseña = trim($_POST['contraseña']);
+    $usuario = trim($_POST['usuario']);
+    $contraseña = trim($_POST['contraseña']);
 
 
     // ELIMINACION DE INJECCION (" OR 1=1#)
-    //$usuario = mysqli_real_escape_string($conn, $usuario);
-    //$contraseña = mysqli_real_escape_string($conn, $contraseña);
+    $usuario = mysqli_real_escape_string($conn, $usuario);
+    $contraseña = mysqli_real_escape_string($conn, $contraseña);
 
 
     // VERIFICAION DE DUPLICACION DE USUARIO
-    //$contejamiento = "SELECT FROM usuarios WHERE usuario=$usuario";
-    //$usuario_verificado = mysqli_query($conexion,$cotejamiento);
+    $contejamiento = "SELECT FROM usuarios WHERE usuario=$usuario";
+    $usuario_verificado = mysqli_query($conexion,$cotejamiento);
 
 
     // ENCRIPTACION DE CONTRASEÑA
-    //$contraseña = password_hash($contraseña, PASSWORD_BCRYPT);
+    $contraseña = password_hash($contraseña, PASSWORD_BCRYPT);
 
 
     // INGRESO DE USUARIO Y CONTRASEÑA A LA BD
-    $consulta = "INSERT INTO 'usuarios' (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña')";
+    $consulta = "INSERT INTO usuarios(usuario, contraseña) VALUES ('$usuario_verificado','$contraseña')";
     $resultado = mysqli_query($conexion,$consulta);
 
     if ($resultado) {
