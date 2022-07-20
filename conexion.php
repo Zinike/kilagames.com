a4f4796fc0cdaa94f13dc17c3ce35b414b69f06b
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-20 10:43:19 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -4,10 +4,11 @@ if(!$conexion){
   echo "Connection error: " . mysqli_connect_error();
 }
 
-$peliculas= "SELECT * FROM peliculas";
-$peliculaslimited= "SELECT * FROM peliculas LIMIT 5";
 
 $pclimited = "SELECT * FROM pc LIMIT 5";
 $pc = "SELECT * FROM pc";
+
+$peliculas= "SELECT * FROM peliculas";
+$peliculaslimited= "SELECT * FROM peliculas LIMIT 5";
 
 ?>
