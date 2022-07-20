030e4c337404ed887747b921adb5150092a5e0ab
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-20 19:14:47 -0300

    ASD

--- config.php
+++ config.php
@@ -3,40 +3,40 @@
 include("conexion.php");
 
 if (isset($_POST['login-register'])) {
-    if (strlen($_POST['usuario']) >= 4 && strlen($_POST['contraseña']) >= 8) {
+  if (strlen($_POST['usuario']) >= 4 && strlen($_POST['contraseña']) >= 8) {
 
-      // ELIMINACION DE ESPACIO CON TRIM
-	    //$usuario = trim($_POST['usuario']);
-	    //$contraseña = trim($_POST['contraseña']);
+    // ELIMINACION DE ESPACIO CON TRIM
+    //$usuario = trim($_POST['usuario']);
+    //$contraseña = trim($_POST['contraseña']);
 
-      // ELIMINACION DE INJECCION (" OR 1=1#)
-      //$usuario = mysqli_real_escape_string($conn, $usuario);
-	    //$contraseña = mysqli_real_escape_string($conn, $contraseña);
+    // ELIMINACION DE INJECCION (" OR 1=1#)
+    //$usuario = mysqli_real_escape_string($conn, $usuario);
+    //$contraseña = mysqli_real_escape_string($conn, $contraseña);
 
-      // VERIFICAION DE DUPLICACION DE USUARIO
-      //$contejamiento = "SELECT FROM usuarios  WHERE usuario=$usuario";
-      //$usuario_verificado = mysqli_query($conexion,$cotejamiento);
+    // VERIFICAION DE DUPLICACION DE USUARIO
+    //$contejamiento = "SELECT FROM usuarios  WHERE usuario=$usuario";
+    //$usuario_verificado = mysqli_query($conexion,$cotejamiento);
 
-      // ENCRIPTACIONB DE CONTRASEÑA
+    // ENCRIPTACIONB DE CONTRASEÑA
 
-      //$contraseña = password_hash($contraseña, PASSWORD_BCRYPT);
+    //$contraseña = password_hash($contraseña, PASSWORD_BCRYPT);
 
-      // INGRESO DE USUARIO Y CONTRASEÑA A LA BD
-	    $consulta = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña')";
-	    $resultado = mysqli_query($conexion,$consulta);
+    // INGRESO DE USUARIO Y CONTRASEÑA A LA BD
+    $consulta = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña')";
+    $resultado = mysqli_query($conexion,$consulta);
 
-	    if ($resultado) {
-	    	?>
-	    	<div class="contenedor"><p>¡Te has registrado correctamente!</p></div>
-        <?php
-	    } else {
-	    	?>
-	    	<div class="contenedor"><p>¡Ups ha ocurrido un error!</p></div>
-        <?php
-	    } else {
-	    ?>
-	    <div class="contenedor"><p>¡Por favor complete los campos!</p></div>
+    if ($resultado) {
+    	?>
+    	<div class="contenedor"><p>¡Te has registrado correctamente!</p></div>
       <?php
+    } else {
+    	?>
+    	<div class="contenedor"><p>¡Ups ha ocurrido un error!</p></div>
+      <?php
+    } else {
+    ?>
+    <div class="contenedor"><p>¡Por favor complete los campos!</p></div>
+    <?php
     }
   }
 }
