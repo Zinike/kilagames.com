c0df6c01c8eb037acb7f657ce9efd0c4c55c0a9a
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 21:37:48 -0300

    asd

--- config.php
+++ config.php
@@ -18,14 +18,12 @@ if (isset($_POST['login-register'])) {
         $cotejamiento = "SELECT * FROM usuarios WHERE usuario='$usuario_clear'";
         $usuario_verificado = mysqli_query($conexion, $cotejamiento);
         $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
-      } else {
-        echo "usuario repetido";
-      }
-    if ($usuario_verificado && $contraseña_codificada){
+        
+    } if ($usuario_verificado && $contraseña_codificada){
         $ingreso_db = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña_codificada')";
         $resultado = mysqli_query($conexion, $ingreso_db);
         if ($resultado){
-          echo "BIENVENIDO '$usuario'";
+          echo "USUARIO Y CONTRASEÑA REGISTRADOS";
         }
     }
   }
