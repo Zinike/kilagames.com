e9b45615b7a69596b2c68fea60e5bca7a38defb1
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 21:31:02 -0300

    asd

--- config.php
+++ config.php
@@ -8,17 +8,17 @@ if (isset($_POST['login-register'])) {
     // ELIMINACION DE ESPACIO CON TRIM
     $usuario = trim($_POST['usuario']);
     $contraseña = trim($_POST['contraseña']);
-    echo "Usuario y Contraseña sin espacios / ";
+
     // ELIMINACION DE INJECCION (" OR 1=1#)
     if ($usuario && $contraseña){
         $usuario_clear = mysqli_real_escape_string($conexion, $usuario);
         $contraseña_clear = mysqli_real_escape_string($conexion, $contraseña);
-        echo "usuario y contraseña limpios / ";
+
     } if ($usuario_clear && $contraseña_clear) {
         $cotejamiento = "SELECT * FROM usuarios WHERE usuario='$usuario_clear'";
         $usuario_verificado = mysqli_query($conexion,$cotejamiento);
         $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
-        echo "usuario unico y contraseña encriptada / ";
+
     } if ($usuario_verificado && $contraseña_codificada){
         $ingreso_db = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario','$contraseña_codificada')";
         $resultado = mysqli_query($conexion, $ingreso_db);
