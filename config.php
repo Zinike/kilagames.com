5cff1cb78d12706c0c0698f210a4ce0ef3a46585
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 21:19:50 -0300

    asd

--- config.php
+++ config.php
@@ -23,9 +23,9 @@ if (isset($_POST['login-register'])) {
         $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
         echo "contraseña encriptada / ";
     } if ($usuario_verificado && $contraseña_codificada){
-        $ingreso_db = "INSERT INTO usuarios (usuario, contraseña) VALUES ($usuario_verificado, $contraseña_codificada)";
+        $ingreso_db = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario_verificado','$contraseña_codificada')";
         $resultado = mysqli_query($conexion, $ingreso_db);
-        if (resultado){
+        if ($resultado){
           echo "USUARIO Y CONTRASEÑA REGISTRADOS";
         }
     }
