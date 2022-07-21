2ea604cafd311939898efb232d3fcececec08aa2
Marcos <marcoszinga_1994@hotmail.com>
2022-07-21 00:30:16 -0300

    asd

--- config.php
+++ config.php
@@ -6,8 +6,11 @@ session_start();
 if (isset($_POST['register'])) {
   if (strlen($_POST['usuario']) >= 3 && strlen($_POST['contraseña']) >= 8) {
 
-    $usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='mysqli_real_escape_string($conexion, trim($_POST['usuario']))'");
-    $contraseña = password_hash(mysqli_real_escape_string($conexion, trim($_POST['contraseña'])), PASSWORD_BCRYPT);
+    $usuarioregistro = mysqli_real_escape_string($conexion, trim($_POST['usuario']));
+    $contraseñaregistro = mysqli_real_escape_string($conexion, trim($_POST['contraseña']));
+
+    $usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuarioregistro'");
+    $contraseña = password_hash($contraseñaregistro, PASSWORD_BCRYPT);
 
     $ingreso = mysqli_query($conexion,"INSERT INTO usuarios (usuario, contraseña) VALUES ("$usuario","$contraseña")");
 
