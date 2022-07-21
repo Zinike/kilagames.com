56ee368076a961273fa95a47ef5f9217dd9d1f67
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 21:14:18 -0300

    ASD

--- config.php
+++ config.php
@@ -19,32 +19,16 @@ if (isset($_POST['login-register'])) {
             $cotejamiento = "SELECT * FROM usuarios WHERE usuario='$usuario_clear'";
             $usuario_verificado = mysqli_query($conexion,$cotejamiento);
             echo "usuario sin repetir / ";
-        } if ($contraseña_clear) {
-            $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
-            echo "contraseña encriptada / ";
-            } if ($usuario_verificado && $contraseña_codificada){
-                $consulta = "INSERT INTO usuarios(usuario, contraseña) VALUES ('$usuario_verificado','$contraseña_codificada')";
-                $resultado = mysqli_query($conexion, $consulta);
-                echo "USUARIO Y CONTRASEÑA REGISTRADOS";
-
-    // RESPUESTAS
-    if ($resultado) {
-    	    ?>
-    	    <div class="contenedor"><p>¡Te has registrado correctamente!</p></div>
-          <?php
-      } else {
-    	   ?>
-    	   <div class="contenedor"><p>¡Ups ha ocurrido un error!</p></div>
-         <?php
-      }
+            } if ($contraseña_clear) {
+                $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
+                echo "contraseña encriptada / ";
+                } if ($usuario_verificado && $contraseña_codificada){
+                    $consulta = "INSERT INTO usuarios(usuario, contraseña) VALUES ('$usuario_verificado','$contraseña_codificada')";
+                    $resultado = mysqli_query($conexion, $consulta);
+                    echo "USUARIO Y CONTRASEÑA REGISTRADOS";
     }
-    } else {
-  ?>
-  <div class="contenedor"><p>¡Por favor complete los campos!</p></div>
-  <?php
   }
 }
-
 // CIERRE DE LA CONEXION
 mysqli_close($conexion);
 ?>
