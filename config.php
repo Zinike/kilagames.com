05f8e364b6d293a719389bdc62bb588828c7b2f7
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 23:37:04 -0300

    asd

--- config.php
+++ config.php
@@ -1,29 +1,38 @@
 <?php
-require "conexion.php";
 
-if (isset($_POST['login-register'])) {
-  if (strlen($_POST['usuario']) >= 3 && strlen($_POST['contraseña']) >= 8) {
+require('conexion.php');
 
-      $usuarioregistro = mysqli_real_escape_string($conexion, trim($_POST['usuario']));
-      $contraseñaregistro = mysqli_real_escape_string($conexion, trim($_POST['contraseña']));
+session_start();
 
-      if ($usuarioregistro && $contraseñaregistro) {
-        $usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuarioregistro'");
-        $contraseña = password_hash($contraseñaregistro, PASSWORD_BCRYPT);
-      } elseif ($usuario && $contraseña) {
-        $ingreso = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario','$contraseña')";
-        $resultado = mysqli_query($conexion, $ingreso);
-        echo "prueba";
-      } elseif ($resultado) {
-          echo "BIENVENIDO, TE HAS REGISTRADO CORRECTAMENTE";
-      } else {
-        echo "NO SE REGISTRO CORRECTAMENTE";
-      }
-    } else {
-      echo "COMPLETA LOS DATOS";
+if (isset($_POST['register'])) {
+
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['contraseña'];
+    $contraseña_hash = contraseña_hash($contraseña, contraseña_BCRYPT);
+
+    $query = $conexion->prepare("SELECT * FROM usuarios WHERE usuario=:usuario");
+    $query->bindParam("usuario", $uduario, PDO::PARAM_STR);
+    $query->execute();
+
+    if ($query->rowCount() > 0) {
+        echo '<p class="error">The email address is already registered!</p>';
+    }
+
+    if ($query->rowCount() == 0) {
+        $query = $connection->prepare("INSERT INTO usuarios(usuario,contraseña) VALUES (:usuario,:contraseña_hash)");
+        $query->bindParam("usuario", $usuario, PDO::PARAM_STR);
+        $query->bindParam("contraseña_hash", $contraseña_hash, PDO::PARAM_STR);
+        $result = $query->execute();
+
+        if ($result) {
+            echo '<p class="success">Your registration was successful!</p>';
+        } else {
+            echo '<p class="error">Something went wrong!</p>';
+        }
     }
 }
 
-
+?>
 mysqli_close($conexion);
 ?>
