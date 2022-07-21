532210908eb442ebf94a42d499a45e77ff07cdd2
Marcos <marcoszinga_1994@hotmail.com>
2022-07-21 00:32:46 -0300

    asd

--- config.php
+++ config.php
@@ -5,17 +5,17 @@ session_start();
 
 if (isset($_POST['register'])) {
   if (strlen($_POST['usuario']) >= 3 && strlen($_POST['contraseña']) >= 8) {
-
+echo "NO SE PUDO!";
     $usuario_registro = mysqli_real_escape_string($conexion, trim($_POST['usuario']));
     $contraseña_registro = mysqli_real_escape_string($conexion, trim($_POST['contraseña']));
-
+echo "NO SE PUDO!";
     $usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario_registro'");
     $ccontraseña = password_hash($contraseña_registro, PASSWORD_BCRYPT);
     echo "NO SE PUDO!";
 
     $ingreso = mysqli_query($conexion,"INSERT INTO usuarios (usuario, contraseña) VALUES ("$usuario","$contraseña")");
     echo "SE PUDO!";
-    
+
     if ($ingreso) {
       echo "BIENVENIDO, TE HAS REGISTRADO CORRECTAMENTE";
     } else {
