3fd4e0978b40dbcbbe2297d20398b8c68f186b54
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 22:55:58 -0300

    ASD

--- config.php
+++ config.php
@@ -17,17 +17,19 @@ if (isset($_POST['login-register'])) {
     if ($usuario && $contraseña) {
         $usuario_clear = mysqli_real_escape_string($conexion, $usuario);
         $contraseña_clear = mysqli_real_escape_string($conexion, $contraseña);
-      } elseif ($usuario_clear && $contraseña_clear) {
+      }
+    if ($usuario_clear && $contraseña_clear) {
         $usuariov = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario_clear'");
         $contraseña_codificada = password_hash($contraseña_clear, PASSWORD_BCRYPT);
-      } elseif ($usuariov && $contraseña_codificada) {
+      }
+    if ($usuariov && $contraseña_codificada) {
         $ingresodb = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuariov','$contraseña_codificada')";
         $resultado = mysqli_query($conexion, $ingresodb);
         echo "prueba";
         if ($resultado){
           echo "USUARIO Y CONTRASEÑA REGISTRADOS";
         } else {
-          echo "Algo malo paso";
+          echo "NO SE REGISTRO CORRECTAMENTE";
         }
     } else {
       echo "Algo esta bien";
