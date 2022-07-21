68070eaf38425a1abb15301f6bbf46ddc967c77a
Marcos <marcoszinga_1994@hotmail.com>
2022-07-20 23:23:11 -0300

    asdasd

--- config.php
+++ config.php
@@ -4,20 +4,19 @@ require "conexion.php";
 if (isset($_POST['login-register'])) {
   if (strlen($_POST['usuario']) >= 3 && strlen($_POST['contraseña']) >= 8) {
 
-      $usuario = mysqli_real_escape_string($conexion, trim($_POST['usuario']));
-      $contraseña = mysqli_real_escape_string($conexion, trim($_POST['contraseña']));
+      $usuarioregistro = mysqli_real_escape_string($conexion, trim($_POST['usuario']));
+      $contraseñaregistro = mysqli_real_escape_string($conexion, trim($_POST['contraseña']));
 
-      if ($usuario && $contraseña) {
-        $usuariov = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario'");
-        $contraseña_codificada = password_hash($contraseña, PASSWORD_BCRYPT);
+      if ($usuarioregistro && $contraseñaregistro) {
+        $usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuarioregistro'");
+        $contraseña = password_hash($contraseñaregistro, PASSWORD_BCRYPT);
       }
-      elseif ($usuariov AND $contraseña_codificada) {
-        $ingreso = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuariov','$contraseña_codificada')";
+      elseif ($usuario && $contraseña) {
+        $ingreso = "INSERT INTO usuarios (usuario, contraseña) VALUES ('$usuario','$contraseña')";
         $resultado = mysqli_query($conexion, $ingreso);
         echo "prueba";
-      }
-      elseif ($resultado) {
-          echo "BIENVENIDO '$usuariov' TE HAS REGISTRADO CORRECTAMENTE";
+      if ($resultado) {
+          echo "BIENVENIDO '$usuario' TE HAS REGISTRADO CORRECTAMENTE";
       }
       else {
         echo "NO SE REGISTRO CORRECTAMENTE";
